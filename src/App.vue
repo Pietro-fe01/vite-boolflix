84e2c6fa3edce6e88a2f249a3f3b8e0c4dc2b59c
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-11-30 11:49:59 +0100

    Creata tutta la struttura base: Cominciamo!

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+
+  export default {
+    components: {
+      HelloWorld
+    }
+  }
+</script>
+
+<template>
+  <HelloWorld/>
+</template>
+
+<style lang="scss">
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<template>
+  ciao
+</template>
+
+<style lang="scss" scoped>
+</style>
